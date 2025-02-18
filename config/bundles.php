954eaf22bdf613dfaefc3afefb02e55895fdbfc9
teodeprati <teodeprati@gmail.com>
2025-02-18 10:43:51 +0100

    Ajout de la fonctionnalité resetpassword

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];
