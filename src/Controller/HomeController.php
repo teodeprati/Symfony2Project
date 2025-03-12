1d654b5f1c651896ecb2ff0d2b615e8bc76db214
teodeprati <teodeprati@gmail.com>
2025-03-12 04:33:45 +0100

    Ajout de mongoDB

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,11 +9,12 @@ use App\Repository\CategorieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Service\MongoDBService;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(ArticleRepository $articleRepository, CategorieRepository $categorieRepository): Response
+    public function index(MongoDBService $mongoDBService, ArticleRepository $articleRepository, CategorieRepository $categorieRepository): Response
     {
         // Récupérer les 4 derniers articles
         $latestArticles = $articleRepository->findBy(
@@ -24,6 +25,8 @@ class HomeController extends AbstractController
 
         // Récupérer toutes les catégories
         $categories = $categorieRepository->findAll();
+
+        $mongoDBService->insertVisit('home');
 
         return $this->render('home/index.html.twig', [
             'latest_articles' => $latestArticles,
